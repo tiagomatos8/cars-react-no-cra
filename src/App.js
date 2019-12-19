1e9d58ff9a778d716a59a745216235e725e88ccc
Tiago Cesar de Morais Matos <tcm.matos@gmail.com>
2019-12-19 19:59:51 +1100

    Formatted App component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link, useHistory } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 
 import Header from './components/Header/Header';
-import YearsPage from './pages/Years/YearsPage';
+import Years from './pages/Years/Years';
 
 const App = () => {
   const history = createBrowserHistory();
-  
+
   return (
     <div className='App-component'>
       <Router>
@@ -15,14 +15,28 @@ const App = () => {
           <Header redirect={history.push} />
 
           <Switch>
-
             {/* If path equals / always redirect to /years path */}
-            <Route exact path='/' render={props => {props.history.push('/years')}} />
+            <Route
+              exact
+              path='/'
+              render={props => {
+                props.history.push('/years');
+              }}
+            />
 
-            <Route exact path='/years' render={props => <YearsPage {...props} />} />
+            {/* Passing Route props to child component, so child can have access to history, location and match */}
+            <Route exact path='/years' render={props => <Years {...props} />} />
 
-            <Route exact path='/vehicles' render={props => <div>vehicles<Link to='/years'>Years</Link></div>} />
-
+            {/* Passing Route props to child component, so child can have access to history, location and match */}
+            <Route
+              exact
+              path='/vehicles'
+              render={props => (
+                <div>
+                  vehicles<Link to='/years'>Years</Link>
+                </div>
+              )}
+            />
           </Switch>
         </>
       </Router>
