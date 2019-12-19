02401f3fd05c93e2fcaaaac01125f26720947a82
Tiago Cesar de Morais Matos <tcm.matos@gmail.com>
2019-12-19 22:12:49 +1100

    Added documentation to button component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+/**
+ * Button - The classic button, in different colors, sizes, and states
+ * https://bulma.io/documentation/elements/button/
+ *
+ * @param {string} label text of the button
+ * @param {boolean} isRounded makes button rounded
+ * @param {boolean} isOutlined makes button outlined
+ * @param {string} icon icon inside the button, using fontAwesome
+ * @param {string} color color of the button https://bulma.io/documentation/elements/button/#colors
+ * @param {string} size size of the button https://bulma.io/documentation/elements/button/#sizes
+ * @param {boolean} isFullwidth make button full width
+ */
 const Button = props => {
   const classNamesObj = {
     isOutlined: props.isOutlined ? 'is-outlined' : '',
     isRounded: props.isRounded ? 'is-rounded' : '',
     color: props.color ? props.color : '',
-    label: props.label ? props.label : 'Button'
+    label: props.label ? props.label : 'Button',
+    size: props.size ? props.size : '',
+    isFullwidth: props.isFullwidth ? 'is-fullwidth' : 'Button'
   };
 
   return (
     <button
-      className={`Button-component button ${classNamesObj.isOutlined} ${classNamesObj.isRounded} ${classNamesObj.color}`}
+      className={`
+        Button-component 
+        button 
+        ${classNamesObj.isOutlined} 
+        ${classNamesObj.isRounded} 
+        ${classNamesObj.color} 
+        ${classNamesObj.size} 
+        ${classNamesObj.isFullwidth}
+      `}
     >
       {props.icon ? (
         <>
@@ -32,7 +54,9 @@ Button.propTypes = {
   isRounded: PropTypes.bool,
   isOutlined: PropTypes.bool,
   icon: PropTypes.string,
-  color: PropTypes.string
+  color: PropTypes.string,
+  size: PropTypes.string,
+  isFullwidth: PropTypes.bool
 };
 
 export default Button;
