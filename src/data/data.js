f44e2cd7eeb988642baab48361d54c64b730aa7e
Tiago Cesar de Morais Matos <tcm.matos@gmail.com>
2019-12-20 22:33:23 +1100

    Changed mock data

--- src/data/data.js
+++ src/data/data.js
@@ -1,26 +1,22 @@
-export const yearsData = () => {
-  return [
-    ['2019', '2018', '2017', '2016'],
-    ['2015', '2014', '2013', '2012'],
-    ['2011', '2010', '2009', "I'll walk"]
-  ];
-}
+export const yearsData = [
+  ['2019', '2018', '2017', '2016'],
+  ['2015', '2014', '2013', '2012'],
+  ['2011', '2010', '2009', "I'll walk"]
+];
 
-export const vehiclesData = () => {
-  return [
-    {
-      model: 'Ascent',
-      series: 'Sport',
-      variant: 'Ascent Sport Sedan 4dr Spts Auto 6sp 2.5i',
-      new_price: 30090,
-      from: 614
-    },
-    {
-      model: 'Ascent',
-      series: 'Sport',
-      variant: 'Ascent Sport Sedan 4dr Spts Auto 6sp 2.5i',
-      new_price: 30590,
-      from: 625
-    }
-  ];
-};
+export const vehiclesData = [
+  {
+    model: 'Ascent',
+    series: 'Sport',
+    variant: 'Ascent Sport Sedan 4dr Spts Auto 6sp 2.5i',
+    new_price: 30090,
+    from: 614
+  },
+  {
+    model: 'Ascent',
+    series: 'Sport',
+    variant: 'Ascent Sport Sedan 4dr Spts Auto 6sp 2.5i',
+    new_price: 30590,
+    from: 625
+  }
+];
